b9d26497e5eedc844bccf82bf70e9711da05a659
edwardconw <coyoteluxeredward@outlook.es>
2019-03-02 22:38:57 -0500

    Menu Auto TXT

--- src/main/java/concesionario/Inicio.java
+++ src/main/java/concesionario/Inicio.java
@@ -5,6 +5,7 @@
  */
 package concesionario;
 import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author SuperUs
@@ -14,11 +15,33 @@ public class Inicio {
     /**
      * @param args the command line arguments
      */
+    public void inicio(){
+       Pruebas pruebaC = new Pruebas();
+        System.out.println("Elija la Opcion: ");
+        System.out.println("*****************");
+        System.out.println("1.  Autos ");
+        System.out.println("2.  Clientes ");
+        System.out.println("3.  Vendedores ");
+        System.out.println("4.  Factura ");       
+        String opcion="";
+        Scanner entScanner = new Scanner (System.in);
+        opcion = entScanner.nextLine (); 
+        int opc = Integer.parseInt(opcion);
+        switch(opc){
+            case 1:
+                Pruebas pru = new Pruebas();
+                pru.menuAuto();
+                break;
+            default:
+                System.out.println("Opcion Invalida");
+                        
+        }
+    }
     public static void main(String[] args) {
        
-        Pruebas pruebaC = new Pruebas();
-        pruebaC.insertarAuto();
-        
+    Inicio i = new Inicio();
+    i.inicio();
+       
     }
     
 }
--- src/main/java/concesionario/Pruebas.java
+++ src/main/java/concesionario/Pruebas.java
@@ -5,56 +5,84 @@
  */
 package concesionario;
 import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author SuperUs
  */
 public class Pruebas {
+ 
     
-    public void insertarAuto(){
-    ArrayList listaAutos = new ArrayList();
-    
-    //Auto 1
-    Auto auto1 = new Auto();
-    auto1.setCodigoCarro("LKJ-999");
-    auto1.setColorCarro("Amarillo");
-    auto1.setMarcaCarro("Toyota");
-    auto1.setTipoCarro("Deportivo");
-    listaAutos.add(auto1);
-    //Auto 2
-    Auto auto2 = new Auto();
-    auto2.setCodigoCarro("GGG-111");
-    auto2.setColorCarro("Rojo");
-    auto2.setMarcaCarro("Mazda");
-    auto2.setTipoCarro("Estandar");
-    listaAutos.add(auto2);
-    //Auto 3
-    Auto auto3 = new Auto();
-    auto3.setCodigoCarro("ZZZ-222");
-    auto3.setColorCarro("Verde");
-    auto3.setMarcaCarro("BMW");
-    auto3.setTipoCarro("Maquinaria");
-    listaAutos.add(auto3);
-    
-    Inventario.crearArchivo(listaAutos);
-    
-    
-    Auto auto4 = new Auto();
-    auto4.setCodigoCarro("DDD-666");
-    auto4.setColorCarro("Cafe");
-    auto4.setMarcaCarro("Ferrary");
-    auto4.setTipoCarro("Maquinaria");
-    ArrayList nuevoAuto = new ArrayList();
-    nuevoAuto.add(auto4);
-    
-    Inventario.aniadirArchivo(nuevoAuto);
-    
-    ArrayList leerListaAuto = new ArrayList<>();
-    leerListaAuto = Inventario.leerArchivo();
-    for(Auto autotxt:(ArrayList<Auto>) leerListaAuto){
-            System.out.println(autotxt.getCodigoCarro()+ " " + autotxt.getColorCarro()+ " " + autotxt.getMarcaCarro()+ " " + autotxt.getTipoCarro());
+    public void menuAuto(){
+            
+        ArrayList listaAutos = new ArrayList(); 
+        System.out.println("Elija La opcion:");
+        System.out.println("****************");
+        System.out.println("1. Ver Autos");
+        System.out.println("2. Insertar Autos");
+        System.out.println("3. Regresar al Menu de inicio");
+        String opcion="";
+        Scanner entScanner = new Scanner (System.in);
+        opcion = entScanner.nextLine (); 
+        int opc = Integer.parseInt(opcion);
+        
+        
+        switch(opc){
+            case 1:
+                   ArrayList leerListaAuto = new ArrayList<>();
+                   leerListaAuto = Inventario.leerArchivo();
+                     for(Auto autotxt:(ArrayList<Auto>) leerListaAuto){
+                         
+                         System.out.println(autotxt.getCodigoCarro()+ " " + autotxt.getColorCarro()+ " " + autotxt.getMarcaCarro()+ " " + autotxt.getTipoCarro());
+                    
+                     }
+                     break;
+            case 2:
+                     //Auto 1
+                        Auto auto1 = new Auto();
+                        auto1.setCodigoCarro("OPG-000");
+                        auto1.setColorCarro("Negro");
+                        auto1.setMarcaCarro("Mazda");
+                        auto1.setTipoCarro("Deportivo");
+                        listaAutos.add(auto1);
+                        //Auto 2
+                        Auto auto2 = new Auto();
+                        auto2.setCodigoCarro("GGG-111");
+                        auto2.setColorCarro("Rojo");
+                        auto2.setMarcaCarro("Mazda");
+                        auto2.setTipoCarro("Estandar");
+                        listaAutos.add(auto2);
+                        //Auto 3
+                        Auto auto3 = new Auto();
+                        auto3.setCodigoCarro("ZZZ-222");
+                        auto3.setColorCarro("Verde");
+                        auto3.setMarcaCarro("BMW");
+                        auto3.setTipoCarro("Maquinaria");
+                        listaAutos.add(auto3);
 
-    }
+                        Inventario.crearArchivo(listaAutos);
+
+
+                        Auto auto4 = new Auto();
+                        auto4.setCodigoCarro("DDD-666");
+                        auto4.setColorCarro("Cafe");
+                        auto4.setMarcaCarro("Ferrary");
+                        auto4.setTipoCarro("Maquinaria");
+                        ArrayList nuevoAuto = new ArrayList();
+                        nuevoAuto.add(auto4);
+
+                        Inventario.aniadirArchivo(nuevoAuto);
+            case 3:
+                Inicio inicio = new Inicio();
+                inicio.inicio();
+            default:
+                System.out.println("Opcion Invalida");
+                
+                }
+   
+        
+    
+ 
     }
     
     
